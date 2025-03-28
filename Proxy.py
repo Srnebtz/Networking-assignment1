4551b464abcea17c39e50adf8439e7c3db3b7618
Srnebtz <s_ht618880@163.com>
2025-03-28 11:32:35 +0800

    Create socket to connect to origin server and connect to IP address

--- Proxy.py
+++ Proxy.py
@@ -128,6 +128,7 @@ while True:
     # Create a socket to connect to origin server
     # and store in originServerSocket
     # ~~~~ INSERT CODE ~~~~
+    originServerSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     # ~~~~ END CODE INSERT ~~~~
 
     print ('Connecting to:\t\t' + hostname + '\n')
@@ -136,6 +137,7 @@ while True:
       address = socket.gethostbyname(hostname)
       # Connect to the origin server
       # ~~~~ INSERT CODE ~~~~
+      originServerSocket.connect((address, 80))
       # ~~~~ END CODE INSERT ~~~~
       print ('Connected to origin Server')
 
